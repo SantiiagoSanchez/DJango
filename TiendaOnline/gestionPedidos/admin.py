3dfa9da7dabb283fe2f4355aa25c060920d80d4b
SantiiagoSanchez <SantiagoSanchezz_21@hotmail.com>
2025-06-12 14:05:54 -0300

    Clase en el fichero admin y modificacion en los models

--- TiendaOnline/gestionPedidos/admin.py
+++ TiendaOnline/gestionPedidos/admin.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from gestionPedidos.models import Clientes, Articulos, Pedidos
 # Register your models here.
 
-admin.site.register(Clientes)
+class ClientesAdmin(admin.ModelAdmin):
+    list_display=("Nombre","Direccion","Telefono")  #Esto es para que se vea en la lista como una nueva seccion.
+    search_fields=("Nombre", "Telefono") #Esto es para habilitar una caja de busqueda por nombre o por telefono.
+
+admin.site.register(Clientes, ClientesAdmin)
 admin.site.register(Articulos)
 admin.site.register(Pedidos)
--- TiendaOnline/gestionPedidos/models.py
+++ TiendaOnline/gestionPedidos/models.py
@@ -4,8 +4,10 @@ class Clientes(models.Model):
     Nombre = models.CharField(max_length=30) #CharField es para decir que es texto lo que se va a guardar
     Direccion = models.CharField(max_length=50)
     Email = models.EmailField(blank=True, null=True) #Esto es para decirle que no es requerido, que puede ser null
-    Telefono = models.CharField(max_length=10)
+    Telefono = models.CharField(max_length=10, verbose_name="Numero de telefono") #verbose_name es para que en el panel de administracion salga el label con ese nombre
 
+    def __str__(self):
+        return self.Nombre
 class Articulos(models.Model):
     Nombre = models.CharField(max_length=30)
     Categoria = models.CharField(max_length=30)
